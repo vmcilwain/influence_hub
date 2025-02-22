f8c91afad1fbc84af162e214bdcde89bf73cd806
Vell McIlwain <vmcilwain@me.com>
2025-02-22 13:24:35 -0500

    add cancel link helper

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,2 +1,7 @@
 module ContactsHelper
+  def cancel_contact_link(contact)
+    link_to :Cancel,
+            (contact.new_record? ? contact.organization : organization_contact_path(contact.organization, contact)),
+            class: :secondary
+  end
 end
