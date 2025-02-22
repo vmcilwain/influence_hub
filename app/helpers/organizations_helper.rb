812aad126b6e12cec2abaadd05aef1da26e2bbc7
Vell McIlwain <vmcilwain@me.com>
2025-02-22 10:55:07 -0500

    add general form styles to organizations

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,2 +1,7 @@
 module OrganizationsHelper
+  def cancel_link(organization)
+    link_to :Cancel,
+            (organization.new_record? ? organizations_path : organization),
+            class: :secondary
+  end
 end
