10c5c847a8192d1b7da38d21eb605e658ade6da8
Vell McIlwain <vmcilwain@me.com>
2025-02-24 12:55:10 -0500

    style contacts area

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -4,4 +4,10 @@ module ContactsHelper
             (contact.new_record? ? contact.organization : organization_contact_path(contact.organization, contact)),
             class: :secondary
   end
+
+  def contact_name(contact)
+    name = "#{contact.first_name} #{contact.last_name}"
+    name += " #{contact.suffix}" if contact.suffix.present?
+    name
+  end
 end
